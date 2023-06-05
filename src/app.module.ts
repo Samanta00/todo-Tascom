088530bd7a7a08b8a7b1d5d3db1eb5995a6fcb7f
Ellen Samanta <ellen.samanta@outlook.com>
2023-06-04 22:51:51 -0300

    feat:database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,9 @@ import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
 import { JwtStrategy } from './auth/jwt.strategy';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Tags } from './tasks/entities/tags.entity';
+import { Task } from './tasks/entities/task.entity';
 
 @Module({
   imports: [
@@ -11,6 +14,22 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
     AuthModule,
     TasksModule,
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DATABASE'),
+        entities: [__dirname + '/../**/*.entity.{ts|js}'],
+        autoLoadEntities: true,
+        logger: 'debug',
+        models: [Tags, Task],
+      }),
+    }),
   ],
   controllers: [],
   providers: [
